924d75f4646b346954259aa2d5125bbcfa93994e
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-10-31 19:39:10 +0530

    adding JS logic and manipulating HTML element text content

--- event_listener1/script.js
+++ event_listener1/script.js
@@ -1,1 +1,16 @@
 
+
+let sel= document.querySelector("select");
+let device=document.querySelector("#device");
+sel.addEventListener("change",function(del)
+
+
+{
+   
+    device.textContent= "Device Selected"
+    device.textContent= `${del.target.value} device is selected` ;
+}
+
+
+);
+
