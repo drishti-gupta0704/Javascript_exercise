98531ea788ece6724588b6fce42876b8e637dd69
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-11-03 21:09:51 +0530

    adding the project logic using JS

--- change_text_on_click/script.js
+++ change_text_on_click/script.js
@@ -1,1 +1,12 @@
 
+
+let heading = document.getElementById("heading");
+let button = document.getElementById("btn");
+
+button.addEventListener("click",function(){
+
+    heading.textContent = "text is changed upon clicking successfuly " ;
+    heading.style.color= " #268b92";
+
+}
+);
