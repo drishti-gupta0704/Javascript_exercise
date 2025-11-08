27df70039e9d9cf5252bab9babd5611291e177d1
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-11-08 21:41:38 +0530

    using fetch API to dynamically convert the currency value

--- currency_converter/script.js
+++ currency_converter/script.js
@@ -1,1 +1,70 @@
 
+// const BASE_URL = "https://api.frankfurter.app/latest?amount=100&from=USD&to=INR";
+
+const dropdowns = document.querySelectorAll(".dropdown select ");
+
+const btn = document.querySelector("form button");
+const fromcurr= document.querySelector(".from select");
+const tocurr= document.querySelector(".to select");
+
+
+// for(code in countryList){
+//     console.log(code,CountryList[code]);
+// }
+
+//adding all the country name in the dropdown list
+// for in loop is used to get indexes
+// for of loop is used to get values
+
+for(let select of dropdowns){
+    for(let currcode in countryList){
+        let newoption = document.createElement("option");
+        newoption.innerText = currcode;
+        newoption.value = currcode;
+        if(select.name === "from" && currcode ==="USD"){
+            newoption.selected = "selected";
+        }
+        else if (select.name === "to" && currcode === "INR"){
+            newoption.selected = "selected"; 
+        }
+
+        select.append(newoption);
+    }
+    select.addEventListener("change",(evt)=>{
+    updateflag(evt.target); //to get selected option from select dropdown
+}
+);
+}
+
+// select.addEventListener("chnage",(evt)=>{
+//     updateflag(evt.target); 
+// );
+
+const updateflag = (element)=> {
+    let currcode= element.value;
+    // console.log(currcode);
+    let countrycode= countryList[currcode];
+    let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newsrc;
+};
+
+btn.addEventListener("click",async(evt)=>
+ { evt.preventDefault();
+let amount = document.querySelector(".amount input");
+let amtval= amount.value;
+if(amtval === "" || amtval <1){
+     amtval = 1;
+    amount.value = "1";
+}
+//API works in lower case only...so make it into lower case
+const URL = `https://api.frankfurter.app/latest?amount=${amtval}&from=${fromcurr.value.toLowerCase()}&to=${tocurr.value.toLowerCase()}` ;
+let response = await fetch(URL);
+console.log(response);
+
+
+ }
+);
+ 
+
+
